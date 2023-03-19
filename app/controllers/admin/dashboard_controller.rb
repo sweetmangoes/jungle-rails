bc437d1368bd10060fffc4fbac38d1c3944c7bc6
sweetmangoes <alphonsechristian@gmail.com>
2023-03-18 22:38:57 -0400

    added stats in stats.html to display stats of categories and products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "jungle", password: "book", except: :index
   def show
+    @category_count = Category.count 
+    @product_count = Product.count
   end
 end
