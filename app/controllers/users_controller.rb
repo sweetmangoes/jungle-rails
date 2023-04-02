07a31d3c47dd30ae16a8f15b21b621e3ae603f2d
sweetmangoes <alphonsechristian@gmail.com>
2023-04-02 13:08:02 -0400

    added error validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,28 @@
 class UsersController < ApplicationController
   
   def new
+    if session[:user_id]
+      redirect_to "/"
+    end 
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    @user.errors.to_hash 
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      # @user.errors.add(:email, "email already been taken")
+      render :new
+      # redirect_to '/signup'
     end    
   end 
+
+  def show
+    @user= User.find(params[:id])
+  end
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ApplicationRecord
-
   has_secure_password
+  validates :name, :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
+  validates :password, confirmation: true
+  
+
+  def self.authenticate_with_credentials(email,password)
+    user_email = email.downcase.strip
+    @user = User.find_by(email: user_email)
+    if @user && @user.authenticate(password)
+      @user
+    elsif 
+      @user.errors.add(:password, "password not matching")
+      nil
+    end
+  end
+
+
 
 end
