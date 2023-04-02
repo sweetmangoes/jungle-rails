9eac964ffae4dd969d915c279be5b74559de5566
sweetmangoes <alphonsechristian@gmail.com>
2023-04-02 19:32:57 -0400

    started product details cypress test

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,13 @@
+describe('product details', () => {
+  
+  // visits homepage
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  // clicks on product partials 
+
+  // product detail page
+
+
+})
