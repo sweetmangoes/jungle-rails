85a6dcbae868022d62a5300cbe8fff324e50794c
sweetmangoes <alphonsechristian@gmail.com>
2023-04-02 15:27:10 -0400

    tested all attr. and name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,10 +6,29 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     it 'is valid with all attributes' do
+      product = Product.new(name: 'Name',
+      description: 'Description',
+      category_id: 1,
+      quantity: 10,
+      image: 'https://images.pexels.com/photos/53435/tree-oak-landscape-view-53435.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: 25.99)
+      expect(product).to be_valid
     end
-    
-    # checks if all is valid
+
     #  validates :name, presence: true
+    it 'is not valid without a name' do
+      product = Product.create(
+        name: nil,
+        description: 'Description',
+        category_id: 1,
+        quantity: 10,
+        image: 'https://images.pexels.com/photos/53435/tree-oak-landscape-view-53435.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price: 25.99)
+
+      expect(product.errors.full_messages[0]).to eq("Name can't be blank")
+    end
+
+
     #  validates :price, presence: true
     #  validates :quantity, presence: true
     #  validates :category, presence: true
